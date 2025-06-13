262cfa5d1641462b0032ad395f9c0fdfbb435cff
Shubham Singodiya <singodiyashubham87@gmail.com>
2025-06-14 01:14:54 +0530

    chore: remove static export since not supported with server actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx', 'server.ts'],
   eslint: {
     ignoreDuringBuilds: true,
   },
